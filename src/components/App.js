5b3a44b8f59132097480d7248c673b34045c85b7
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-10-26 14:25:43 +0530

    add postion

--- src/components/App.js
+++ src/components/App.js
@@ -10,62 +10,71 @@ const App = () => {
     top: "0px"
   });
   const handleStart = () => {
-    console.log("handle start");
-    setRenderBall(true);
-    renderChoice();
+    //console.log("handle start");
+    let newBall = true;
+    setRenderBall(newBall);
   };
 
   const handleBallPosition = (event) => {
-    console.log(event);
+    console.log(event.code);
     if (event.keyCode === 37) {
       // left - change only x
-      const leftTemp = x + 5;
+      const leftTemp = x - 5;
       setX(leftTemp);
+      setBallPosition({ left: `${leftTemp}px`, right: `${y}px` });
     } else if (event.keyCode === 39) {
       //right - change only x
-      const rightTemp = x - 5;
+      const rightTemp = x + 5;
       setX(rightTemp);
+      setBallPosition({ left: `${rightTemp}px`, right: `${y}px` });
     } else if (event.keyCode === 38) {
       // top - change only y
-      const topTemp = y + 5;
+      const topTemp = y - 5;
       setY(topTemp);
+      setBallPosition({ left: `${x}px`, right: `${topTemp}px` });
     } else if (event.keyCode === 40) {
       //down - change only y
-      const downTemp = y - 5;
+
+      const downTemp = y + 5;
+      setBallPosition({ left: `${x}px`, right: `${downTemp}px` });
       setY(downTemp);
     }
-
-    setBallPosition({
-      left: x + "px",
-      top: y + "px"
-    });
   };
-  document.addEventListener("keydown", handleBallPosition);
+
   const reset = () => {
-    setRenderBall(false);
-    renderChoice();
+    let resetX = 0;
+    let resetY = 0;
+    let resetBall = false;
+    let resetLeft = 0;
+    let resetRight = 0;
+
+    setX(resetX);
+    setY(resetY);
+    setRenderBall(resetBall);
+    setBallPosition({ left: resetLeft, right: resetRight });
   };
   const renderChoice = () => {
-    if (renderBall === false) {
-      console.log("render button");
-      return (
-        <button className="start" onClick={handleStart}>
-          start
-        </button>
-      );
-    } else {
-      console.log("render ball");
-
-      return <div className="ball"></div>;
-    }
+    document.addEventListener("keydown", (event) => handleBallPosition(event));
   };
-
   return (
     <div className="playground">
-      {renderChoice()}
+      {!renderBall && (
+        <button onClick={handleStart} className="start">
+          Start
+        </button>
+      )}
+
+      {renderBall && (
+        <div
+          className="ball"
+          style={{ left: ballPosition.left, top: ballPosition.right }}
+        ></div>
+      )}
+
       <button onClick={reset} className="reset">
         Reset
       </button>
+      {renderChoice()}
     </div>
   );
 };
