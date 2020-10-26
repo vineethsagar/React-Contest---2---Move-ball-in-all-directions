4a02c091b682f1687c689883044b7c10a01f08bc
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-10-26 13:05:23 +0530

    updated position

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,67 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const handleStart = () => {
+    console.log("handle start");
+    setRenderBall(true);
+    renderChoice();
+  };
+
+  const handleBallPosition = (event) => {
+    console.log(event);
+    if (event.keyCode === 37) {
+      // left - change only x
+      const leftTemp = x + 5;
+      setX(leftTemp);
+    } else if (event.keyCode === 39) {
+      //right - change only x
+      const rightTemp = x - 5;
+      setX(rightTemp);
+    } else if (event.keyCode === 38) {
+      // top - change only y
+      const topTemp = y + 5;
+      setY(topTemp);
+    } else if (event.keyCode === 40) {
+      //down - change only y
+      const downTemp = y - 5;
+      setY(downTemp);
+    }
+
+    setBallPosition({
+      left: x + "px",
+      top: y + "px"
+    });
+  };
+  document.addEventListener("keydown", handleBallPosition);
+  const reset = () => {
+    setRenderBall(false);
+    renderChoice();
+  };
+  const renderChoice = () => {
+    if (renderBall === false) {
+      console.log("render button");
+      return (
+        <button className="start" onClick={handleStart}>
+          start
+        </button>
+      );
+    } else {
+      console.log("render ball");
+
+      return <div className="ball"></div>;
+    }
+  };
 
   return (
     <div className="playground">
+      {renderChoice()}
       <button onClick={reset} className="reset">
         Reset
       </button>
-      {renderChoice()}
     </div>
   );
 };
